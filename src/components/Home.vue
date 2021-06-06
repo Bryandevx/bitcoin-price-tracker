45bd95b4dfd2a39947980df0b20c8930c4c6e385
Bryan Solano <bryan99.solano.saborio@gmail.com>
2021-06-05 21:43:53 -0600

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p v-if="dataReady">
+        {{data}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      data: "",
+      dataReady: false,
+    };
+  },
+   async mounted() {
+       const response = await this.$axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        this.data = JSON.stringify(response.data.bpi)
+        this.dataReady = true
+  },
+
+  methods: {
+    async fetchData() {
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
